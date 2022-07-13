583457881ed23e7b2707f906acec8dc863f966f8
Matheus Magalhães <matheus.magalha95@gmail.com>
2022-07-13 00:03:55 -0300

    Project created

---
+++ dsmeta/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
